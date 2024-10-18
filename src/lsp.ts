51257a976065c70fb5620c2de32452104b2bbf22
Chris Chinchilla <chris@chrischinchilla.com>
2024-10-18 15:42:53 +0200

    Add filters to replicate spell check and min alert level features

--- src/lsp.ts
+++ src/lsp.ts
@@ -2,12 +2,11 @@ import { workspace, ExtensionContext } from "vscode";
 import * as vscode from "vscode";
 
 import {
-	LanguageClient,
-	LanguageClientOptions,
-	ServerOptions,
-	TransportKind
-} from 'vscode-languageclient/node';
-
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
 
 let client: LanguageClient;
 
@@ -15,11 +14,15 @@ const TAG = "v0.3.8";
 const URL =
   "https://github.com/errata-ai/vale-ls/releases/download/{tag}/vale-ls-{arch}-{platform}.zip";
 
-  type valeConfigOptions = "configPath" | "syncOnStartup" | "filter" | "installVale";
+type valeConfigOptions =
+  | "configPath"
+  | "syncOnStartup"
+  | "filter"
+  | "installVale";
 
-  interface valeArgs {
-    value: string;
-  }
+interface valeArgs {
+  value: string;
+}
 
 export function getArch(): String {
   if (process.arch == "x64") return "x86_64";
@@ -41,27 +44,56 @@ export function activate(context: ExtensionContext) {
 
   // TODO: Factor in https://vale.sh/docs/integrations/guide/#vale-ls
   // Has the user defined a config file manually?
-const configuration = vscode.workspace.getConfiguration();
-//   let customConfigPath = configuration.get<string>("vale.valeCLI.config");
-//   if (customConfigPath) {
-//     console.log("Using config: " + customConfigPath);
+  const configuration = vscode.workspace.getConfiguration();
+  // Make global constant for now as will reuse and build upon later
+  let valeFilter: valeArgs = { value: "" };
+  let filters: string[] = [];
+  //   let customConfigPath = configuration.get<string>("vale.valeCLI.config");
+  //   if (customConfigPath) {
+  //     console.log("Using config: " + customConfigPath);
 
-//   valeArgs = {configPath: customConfigPath};
-//   }
-// console.log(valeArgs)
+  //   valeArgs = {configPath: customConfigPath};
+  //   }
+  // console.log(valeArgs)
   console.log("Using  binary: " + valePath);
-let valeConfig: Record<valeConfigOptions, valeArgs> = {
-  configPath: configuration.get("vale.valeCLI.configPath") as valeArgs,
-  syncOnStartup: configuration.get("vale.valeCLI.syncOnStartup") as valeArgs,
-  filter: configuration.get("vale.valeCLI.filter") as valeArgs,
-  installVale: configuration.get("vale.valeCLI.installVale") as valeArgs,
-}
-console.log(valeConfig)
-// TODO: So do I need the below?
-let tempArgs: never[] = [];
+
+  // Handle old minAlertLevel to output as filter
+  if (configuration.get("vale.valeCLI.minAlertLevel") !== "inherited") {
+    let minAlertLevel = configuration.get("vale.valeCLI.minAlertLevel");
+
+    if (minAlertLevel === "suggestion") {
+      filters.push(`.Level in ["suggestion", "warning", "error"]`);
+    }
+    if (minAlertLevel === "warning") {
+      filters.push(`.Level in ["warning", "error"]`);
+    }
+    if (minAlertLevel === "error") {
+      filters.push(`.Level in ["error"]`);
+    }
+  }
+
+    // Handle old enableSpellcheck to output as filter
+    if (configuration.get("vale.enableSpellcheck") === false) {
+      filters.push(`.Extends != "spelling"`);
+    }
+
+    // Create combined filters
+    if (filters.length > 0) {
+      valeFilter = filters.join(" and ") as unknown as valeArgs;
+    }
+
+  let valeConfig: Record<valeConfigOptions, valeArgs> = {
+    configPath: configuration.get("vale.valeCLI.configPath") as valeArgs,
+    syncOnStartup: configuration.get("vale.valeCLI.syncOnStartup") as valeArgs,
+    filter: valeFilter as unknown as valeArgs,
+    installVale: configuration.get("vale.valeCLI.installVale") as valeArgs,
+  };
+  console.log(valeConfig);
+  // TODO: So do I need the below?
+  let tempArgs: never[] = [];
   let serverOptions: ServerOptions = {
-    run: { command: valePath, args: tempArgs},
-    debug: { command: valePath, args: tempArgs},
+    run: { command: valePath, args: tempArgs },
+    debug: { command: valePath, args: tempArgs },
   };
 
   // Options to control the language client
@@ -72,7 +104,7 @@ let tempArgs: never[] = [];
     synchronize: {
       fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
     },
-    };
+  };
 
   // Create the language client and start the client.
   client = new LanguageClient(
